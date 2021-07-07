df3bbf1df521c8f90285f28cf17b7d6f5af1c866
athulyasanthosh <athulyasanthosh@users.noreply.github.com>
2021-07-07 12:34:54 +0000

    Fix styling

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -6,35 +6,38 @@ use Athulya\LaravelKnowledgeBase\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
     public function index(Request $request)
     {
         //dd($request->category_id);
         $articleList = '';
         $categories = '';
-        if(isset($request->category_id) && isset($request->keyword)) {
+        if (isset($request->category_id) && isset($request->keyword)) {
             $articleList = Article::where('category_id', $request->category_id)
                         ->where('article_name', 'like', '%'.$request->keyword.'%')
                         ->get();
-        } else if(isset($request->keyword)) {
+        } elseif (isset($request->keyword)) {
             $articleList = Article::where('article_name', 'like', '%'.$request->keyword.'%')
                         ->get();
-        } else if(isset($request->category_id)) {
+        } elseif (isset($request->category_id)) {
             $articleList = Article::where('category_id', $request->category_id)
                         ->get();
-        } 
-        $categories = Category::latest()->get(); 
+        }
+        $categories = Category::latest()->get();
         $latestArticle = Article::orderBy('created_at', 'DESC')
                         ->limit(5)
-                        ->get();                        
+                        ->get();
+
         return view('knowledge-base::home', compact('categories', 'articleList', 'latestArticle'));
     }
 
-    public function articleDetail($slug) {
+    public function articleDetail($slug)
+    {
         dd($slug);
-       $article = Article::where('slug', $slug)->first();
-       dd($article);
-       return view('knowledge-base::details', compact('article'));
+        $article = Article::where('slug', $slug)->first();
+        dd($article);
+
+        return view('knowledge-base::details', compact('article'));
     }
 }
-?>
