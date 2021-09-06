75afe72bc68a7ea68a11183615da3dcea22728bf
athulyasanthosh <athulyasanthosh@users.noreply.github.com>
2021-09-06 09:15:03 +0000

    Fix styling

--- src/LaravelKnowledgeBaseServiceProvider.php
+++ src/LaravelKnowledgeBaseServiceProvider.php
@@ -3,10 +3,6 @@
 namespace Athulya\LaravelKnowledgeBase;
 
 use Athulya\LaravelKnowledgeBase\Commands\LaravelKnowledgeBaseCommand;
-use Athulya\LaravelKnowledgeBase\Http\Livewire\Articles;
-use Athulya\LaravelKnowledgeBase\Http\Livewire\Categories;
-use Athulya\LaravelKnowledgeBase\Http\Livewire\Details;
-use Athulya\LaravelKnowledgeBase\Http\Livewire\FrontendKnowledgeBase;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -28,5 +24,5 @@ class LaravelKnowledgeBaseServiceProvider extends PackageServiceProvider
             ->hasMigration('create_article_table')
             ->hasMigration('create_category_table')
             ->hasCommand(LaravelKnowledgeBaseCommand::class);
-    }    
+    }
 }
